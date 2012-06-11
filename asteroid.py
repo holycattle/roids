891bbe0ad82e09b6d08270fe4d5f68048e0eb9ba
jlibasco <jlibasco@gmail.com>
2012-06-12 01:52:48 +0800

    Added all current components to main_batch for easier screen updating; added "life" icons

--- asteroid.py
+++ asteroid.py
@@ -3,20 +3,19 @@ from game import resources, load
 
 game_window = pyglet.window.Window(800, 600)
 
-score_label = pyglet.text.Label(text="Score: 0", x=10, y=575)
-level_label = pyglet.text.Label(text="Game 1", x=400, y=575, anchor_x='center')
+main_batch = pyglet.graphics.Batch()
 
-player_ship = pyglet.sprite.Sprite(img = resources.player_image, x=400, y=300)
-asteroids = load.asteroids(3, player_ship.position)
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
+level_label = pyglet.text.Label(text="Game 1", x=400, y=575, anchor_x='center', batch=main_batch)
+
+player_ship = pyglet.sprite.Sprite(img = resources.player_image, x=400, y=300, batch=main_batch)
+asteroids = load.asteroids(3, player_ship.position, main_batch)
+lives = load.player_lives(2, main_batch)
 
 @game_window.event
 def on_draw():
 	game_window.clear()
-	level_label.draw()
-	score_label.draw()
-	player_ship.draw()
-	for asteroid in asteroids:
-		asteroid.draw()
+	main_batch.draw()
 
 if __name__ == '__main__':
 	pyglet.app.run()
--- game/load.py
+++ game/load.py
@@ -1,17 +1,25 @@
 import pyglet, random, math
 import resources
 
-def asteroids(num_asteroids, player_position):
+def asteroids(num_asteroids, player_position, batch=None):
 	asteroids = []
 	for i in range(num_asteroids):
 		asteroid_x, asteroid_y = player_position
 		while get_distance((asteroid_x, asteroid_y), player_position) < 100:
 			asteroid_x = random.randint(0, 800)
 			asteroid_y = random.randint(0, 600)
-		new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image, x=asteroid_x, y=asteroid_y)
+		new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image, x=asteroid_x, y=asteroid_y, batch=batch)
 		new_asteroid.rotation = random.randint(0, 360)
 		asteroids.append(new_asteroid)
 	return asteroids
+
+def player_lives(num_icons, batch=None):
+	player_lives = []
+	for i in range(num_icons):
+		new_sprite = pyglet.sprite.Sprite(img=resources.player_image, x=785-i*30, y=585, batch=batch)
+		new_sprite.scale = 0.5
+		player_lives.append(new_sprite)
+	return player_lives
 
 def get_distance(p1=(0, 0), p2=(0,0)):
 	return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
